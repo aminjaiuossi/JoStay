150e2879a9c067e3ec3f3b928024fb87dcb825de
Amin <jaiuossijob@gmail.com>
2025-06-08 16:51:06 +0300

    commit two

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
      mongoose.connection.on('connected', () => console.log("DataBase Connected"));
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(`${process.env.MONGODB_URI}/JoStay`);
 
     }
   catch (error) {
